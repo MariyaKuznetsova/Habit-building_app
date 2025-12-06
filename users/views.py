a8dcec7576552594a3d8c9588d439e4892706bf5
MariyaKuznetsova <piratca93@mail.ru>
2025-12-07 01:33:28 +0400

    kursovaya_5

---
+++ users/views.py
@@ -0,0 +1,21 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter, SearchFilter
+from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.permissions import IsAuthenticated
+
+from users.models import User
+
+from .serializers import UserSerializer
+
+
+class UserCreateAPIView(CreateAPIView):
+    """Контроллер для пользователя"""
+
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
+
